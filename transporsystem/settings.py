eb8501b94d1bdfe415653c4dd0dde1bd66198bab
Ian Estacio <mimestacio@tip.edu.ph>
2025-05-18 20:19:54 +0800

    fixed settings.py typo

--- transporsystem/settings.py
+++ transporsystem/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
 ]
 
 
-ROOT_URLCONF = 'transporsystem.urls'
+ROOT_URLCONF = 'transposystem.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'transporsystem.wsgi.application'
+WSGI_APPLICATION = 'transposystem.wsgi.application'
 
 # Database configuration - Modified for better Djongo compatibility
 DATABASES = {
